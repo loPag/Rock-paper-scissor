52f69d5cda83719147f4c465d42cbe80bd86d3be
loPag <68325623+loPag@users.noreply.github.com>
2022-07-09 13:54:57 +0200

    1st step project script done

--- script.js
+++ script.js
@@ -1,57 +1,59 @@
 // Script Rock Paper Scissor
 
+for(let i=0; i<5; i++) {
 
 //ComputerPlay Function
 function computerPlay(max) {
     //it returns one of three numbers: 0, 1 or 2
     return Math.floor(Math.random()*max);
 }
-// 0 mean Scissor
+// computerSelection store the computer shot: 0=Scissor, 1=paper, 2=rock
     let computerSelection = computerPlay(3)
-    
-    if (computerSelection === 0) {
-        console.log("Scissor!");
-    }
-// 1 mean Paper
-    if (computerSelection === 1) {
-        console.log("Paper!");
-    }
-// 2 mean Rock
-    if (computerSelection === 2) {
-        console.log("Rock!")
-    }
+   
 
 
 //Opening a Window for storing the user input
-let playerInput= prompt("Enter: Paper, Rock or Scissor");
+    let playerInput= prompt("Enter: Paper, Rock or Scissor");
 
 //Storing the user input in the variable playerSet in lower case characters
-let playerSelection= playerInput.toLocaleLowerCase();
+    let playerSelection= playerInput.toLocaleLowerCase();
 
-if (computerSelection===0 && playerSelection=='paper') {
-    console.log("Computer win");
+
+//This function match the shot from Computer to the user Selection and return a result
+    function playRound(playerSelection, computerSelection) {
+
+    let result;
+
+        if (computerSelection===0 && playerSelection=='paper') {
+            result="Computer win";
+        }
+        if (computerSelection===0 && playerSelection=='rock') {
+             result="User Win";
+        }
+        if (computerSelection===0 && playerSelection=='scissor') {
+             result="Pair";
+        }
+        if (computerSelection===1 && playerSelection=='rock') {
+             result="Computer win";
+        }
+        if (computerSelection===1 && playerSelection=='scissor') {
+             result="User Win";
+        }
+        if (computerSelection===1 && playerSelection=='paper') {
+             result="Pair";
+        }
+        if (computerSelection===2 && playerSelection=='scissor') {
+             result="Computer win";
+        }
+        if (computerSelection===2 && playerSelection=='paper') {
+             result="User Win";
+        }
+        if (computerSelection===2 && playerSelection=='rock') {
+             result="Pair";
+        }
+    return result;
 }
-if (computerSelection===0 && playerSelection=='rock') {
-    console.log("User Win");
-}
-if (computerSelection===0 && playerSelection=='scissor') {
-    console.log("Pair");
-}
-if (computerSelection===1 && playerSelection=='rock') {
-    console.log("Computer win");
-}
-if (computerSelection===1 && playerSelection=='scissor') {
-    console.log("User Win");
-}
-if (computerSelection===1 && playerSelection=='paper') {
-    console.log("Pair");
-}
-if (computerSelection===2 && playerSelection=='scissor') {
-    console.log("Computer win");
-}
-if (computerSelection===2 && playerSelection=='paper') {
-    console.log("User Win");
-}
-if (computerSelection===2 && playerSelection=='rock') {
-    console.log("Pair");
+
+console.log(playRound(playerSelection, computerSelection));
+
 }
